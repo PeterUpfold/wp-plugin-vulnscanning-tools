e34c13f339feadcb078b50f19c5abe2e8aca7682
PeterUpfold <pgithub@upfold.org.uk>
2020-03-21 22:43:18 +0000

    Installs per plugin

--- determine-target-plugins.py
+++ determine-target-plugins.py
@@ -22,9 +22,12 @@
 
 
 from html.parser import HTMLParser
+import lxml # don't ask me why I'm parsing HTML two different ways. I had some existing code, all right? :)
 import argparse
 import os
 import re
+import requests
+from time import sleep
 
 
 # argument parsing
@@ -35,6 +38,7 @@ argparser.add_argument('--force', dest='force', help='Allow this script to overw
 
 args = argparser.parse_args()
 
+headers = {'User-Agent', 'determine-target-plugins.py +https://github.com/PeterUpfold/wp-plugin-vulnscanning-tools/'}
 
 # * for each top level plugin slug, pull the https://wordpress.org/plugins/{name} page and parse number of installations
 # * Decide which plugins to target based on user base
@@ -48,9 +52,12 @@ class RootSVNPageParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
             slug = attrs[0][1]
-            if self.slug_regex.search(slug):
-                print('Adding slug ', slug)
-                self.slugs.append(slug)
+            self.slugs.append(slug)
+
+            # the regex causes huge slowdowns ~>12,500 items. No idea why...
+            #if self.slug_regex.search(slug):
+            #    print('Adding slug ', slug)
+            #    self.slugs.append(slug)
 
     def handle_endtag(self, tag):
         #print ("end", tag)
@@ -64,6 +71,21 @@ svn_page_parser = RootSVNPageParser()
 
 svn_page_parser.feed(args.inputfile.read())
 
+plugins = {} # store our overall state
+
 for slug in svn_page_parser.slugs:
     # go get the plugin page. We'll need to pull the number of installs
-    print('Would handle ', slug)
+    print('Pulling page for ', slug)
+
+    plugin_page = requests.get('https://wordpress.org/plugins/' + slug + '/', headers=headers)
+    if plugin_page.status_code == 200:
+        tree = lxml.html.fromstring(plugin_page.content)
+        installs = tree.xpath('/html/body/div[3]/div/main/article/div[3]/div[1]/ul/li[3]/strong/text()')
+
+        print(slug + ' has ' + installs)
+    else:
+        print('Failed on ' + slug + ' with error ' + str(plugin_page.status_code))
+
+
+    print('Finished with ', slug)
+    sleep(3) # robots.txt for plugins.svn.wordpress.org indicates this
